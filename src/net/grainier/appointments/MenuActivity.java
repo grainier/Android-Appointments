b9dedcecf32f5d014ca080e6707a3215b3d63876
Grainier Perera <ivone.perera@gmail.com>
2013-04-11 14:40:30 +0530

    Update MenuActivity.java

--- src/net/grainier/appointments/MenuActivity.java
+++ src/net/grainier/appointments/MenuActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 
 public class MenuActivity extends Activity {
 
+	// Adding  a line of comment
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
