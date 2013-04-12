2257eaffacbd92d1102ab2fca641b290e393728d
grainier <ivone.perera@gmail.com>
2013-04-12 21:39:29 +0530

    Appointment ID attribute added

--- src/net/grainier/appointments/Appointment.java
+++ src/net/grainier/appointments/Appointment.java
@@ -1,33 +1,50 @@
 package net.grainier.appointments;
 
 public class Appointment {
-	private String title;	
+	
+	private int id;
+	private String title;
+	private String date;
+	private String time;	
 	private String details;
-	private String time;
-	private String date;	
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
 	
 	public String getTitle() {
 		return title;
 	}
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public String getDetails() {
-		return details;
-	}
-	public void setDetails(String details) {
-		this.details = details;
-	}
-	public String getTime() {
-		return time;
-	}
-	public void setTime(String time) {
-		this.time = time;
-	}
+	
 	public String getDate() {
 		return date;
 	}
+	
 	public void setDate(String date) {
 		this.date = date;
 	}
+	
+	public String getTime() {
+		return time;
+	}
+	
+	public void setTime(String time) {
+		this.time = time;
+	}
+	
+	public String getDetails() {
+		return details;
+	}
+	
+	public void setDetails(String details) {
+		this.details = details;
+	}	
 }
