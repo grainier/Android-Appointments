1410336bc58115f6b3d349b398ad686bd3ccdb61
grainier <ivone.perera@gmail.com>
2013-04-12 12:29:48 +0530

    Appointment Class added

---
+++ src/net/grainier/appointments/Appointment.java
@@ -0,0 +1,26 @@
+package net.grainier.appointments;
+
+public class Appointment {
+	private String title;	
+	private String details;
+	private String time;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+}
